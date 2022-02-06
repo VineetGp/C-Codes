76ebb5b5c637e38e4aaf7f6fd3e1f784f09367d3
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-02-06 20:42:17 +0530

    Update 26-remove-duplicates-from-sorted-array.cpp

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -13,3 +13,20 @@ public:
         return j+1;
     }
 };
+
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int j = 0;
+        if(nums.size()==0)
+            return 0;
+        for(int i = 0; i<nums.size();i++){
+            if(nums[j]==nums[i])
+                continue;
+            else
+                nums[j+1]=nums[i];
+                j++;
+        }
+        return j+1;
+    }
+};
