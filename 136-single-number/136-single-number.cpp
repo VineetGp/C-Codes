6108328349e8fbc8bb5e992d42f2dfe6e01c5684
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-03-23 16:31:00 +0530

    Time: 16 ms (92.52%), Space: 16.9 MB (85.45%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -1,14 +1,10 @@
 class Solution {
 public:
     int singleNumber(vector<int>& nums) {
-        if(nums.size()==1){
-            return nums[0];
+        int res = 0;
+        for(int i=0; i<nums.size(); i++){
+            res = res^nums[i];
         }
-        int a;
-        a = nums[0]^nums[1];
-        for(int i=2; i<nums.size(); i++){
-            a = a^nums[i];
-        }
-        return a;
+        return res;
     }
 };
