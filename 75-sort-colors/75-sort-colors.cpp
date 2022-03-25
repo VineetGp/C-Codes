65681aaf2709e401540b1e4f388772fcf3ec3182
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-03-25 15:38:35 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (90.84%) - LeetHub

---
+++ 75-sort-colors/75-sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void sortColors(vector<int>& nums) {
+        int ones = 0, zeros = 0, twos = 0;
+        for(int i:nums){
+            if(i==0) zeros++;
+            else if(i==1) ones++;
+            else twos++;
+        }
+        cout<<zeros;
+        for(int i=0; i<zeros; i++){
+            nums[i] = 0;
+        }
+        for(int i=zeros; i<ones+zeros; i++){
+            nums[i] = 1;
+        }
+        for(int i=ones+zeros; i<nums.size(); i++){
+            nums[i] = 2;
+        }
+    }
+};
