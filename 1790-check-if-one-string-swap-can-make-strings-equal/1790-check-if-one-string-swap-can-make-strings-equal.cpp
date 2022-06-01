4267d96b4436c53158a882c4797d3e45966aea09
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-06-02 01:27:55 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (23.43%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -2,14 +2,16 @@ class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
         if(s1.size()!=s2.size()) return false;
-        if(s1==s2) return true;
-        vector<int> diff;
+        int idx1 = -1, idx2 = -1;
+        int diff = 0;
         for(int i = 0; i<s1.size(); i++){
             if(s1[i]!=s2[i]){
-                diff.push_back(i);
-                if(diff.size()>2) return false;
+                diff++;
+                if(idx1 == -1) idx1 = i;
+                else idx2 = i;
             }
+            if(diff>2) return false;
         }
-        return diff.size()==2 && s1[diff[0]] == s2[diff[1]] && s1[diff[1]] == s2[diff[0]];
+        return diff == 0 || (diff==2 && s1[idx1] == s2[idx2] && s1[idx2] == s2[idx1]);
     }
 };
