458baa3118b457baae61de4b6dc9b13bebced352
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-06-02 13:40:24 +0530

    Time: 20 ms (27.23%), Space: 10.6 MB (43.03%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.cpp
+++ 867-transpose-matrix/867-transpose-matrix.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
@@ -10,5 +11,22 @@ public:
             res.push_back(temp);
         }
         return res;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        
+        int m=matrix.size();
+        int n=matrix[0].size();
+        vector<vector<int>>ans(n,vector<int>(m,0));
+        for(int j=0;j<n;j++){
+            for(int i=0;i<m;i++){
+                ans[j][i]=matrix[i][j];
+            }
+        }
+        return ans;
     }
 };
