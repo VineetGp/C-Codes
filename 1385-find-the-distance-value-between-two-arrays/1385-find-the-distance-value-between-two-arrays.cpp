99a2a2b0f159fc230027aa249bfe229e7fabb542
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-06-05 00:35:08 +0530

    Time: 21 ms (32.91%), Space: 13.2 MB (29.75%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -1,8 +1,7 @@
 /*
 Reference:
 https://leetcode.com/problems/find-the-distance-value-between-two-arrays/discuss/1299485/C%2B%2B-2-Pointers%3A-Brute-Force%3A-O(N.M)-and-Binary-Search%3A-O(N.logM)
-*/
-
+****** Using brute force approach that is double for loop *******
 class Solution {
 public:
     int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
@@ -16,5 +15,31 @@ public:
             }
         }
         return res;
+    }
+};
+*/
+
+class Solution {
+public:
+    int findTheDistanceValue(vector<int>& arr1, vector<int>& arr2, int d) {
+        sort(arr2.begin(), arr2.end());
+        int count = 0;
+        for(int i = 0; i<arr1.size(); i++){
+            bool temp = false;
+            int l = 0;
+            int r = arr2.size()-1;
+            int m = -1;
+            while(l<=r){
+                m = (l+r)/2;
+                if(abs(arr1[i]-arr2[m])<=d){
+                    temp = true;
+                    break;
+                }
+                else if(arr2[m]>arr1[i]) r = m-1;
+                else l = m+1;
+            }
+            if(!temp) count++;
+        }
+        return count;
     }
 };
