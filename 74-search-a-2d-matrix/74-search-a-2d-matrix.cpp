3791008d4e5cbba74a5002f14957a5073dcf19cd
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-06-24 02:31:58 +0530

    Time: 12 ms (12.33%), Space: 9.6 MB (51.94%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
@@ -15,6 +16,29 @@ public:
                 else if(matrix[r][m]<target) start = m+1;
                 else end = m-1;
             }
+        }
+        return false;
+    }
+};
+*/
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) 
+    {
+        int n = matrix.size();
+        int m = matrix[0].size();
+        int begin = 0, end = n*m-1;
+        while(begin<=end)
+        {
+            int mid = (begin + end)/2;
+            int mid_val = matrix[mid/m][mid%m];
+            if(mid_val==target)
+                return true;
+            else if(mid_val<target)
+                begin = mid+1;
+            else
+                end = mid-1;
         }
         return false;
     }
