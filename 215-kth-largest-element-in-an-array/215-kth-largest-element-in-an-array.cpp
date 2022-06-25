b99cd7f1da407e9d7942bc10851025c5d35c5402
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-06-25 18:23:14 +0530

    Time: 4 ms (96.89%), Space: 10.2 MB (36.80%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.cpp
@@ -1,3 +1,8 @@
+/*
+https://leetcode.com/problems/kth-largest-element-in-an-array/discuss/60309/C%2B%2B-STL-partition-and-heapsort
+*/
+/*
+O(nlogn)
 class Solution {
 public:
     bool static comp(int a , int b){
@@ -9,5 +14,16 @@ public:
         }
         sort(nums.begin(), nums.end(), comp);
         return nums[k-1];
+    }
+};
+*/
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq(nums.begin(), nums.end());
+        for(int i = 0; i<k-1; i++){
+            pq.pop();
+        }
+        return pq.top();
     }
 };
