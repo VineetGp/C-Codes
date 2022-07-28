5698141814cb4e093336aa521d2436bf48d4627e
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-07-28 20:11:52 +0530

    Time: 181 ms (55.04%), Space: 61.2 MB (91.01%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,17 +1,23 @@
+/*
+https://www.youtube.com/watch?v=32Ll35mhWg0&ab_channel=takeUforward
+https://www.youtube.com/watch?v=wjYnzkAhcNk&ab_channel=NeetCode
+*/
+
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int i = 0;
-        while(i<nums.size()){
-            if(nums[i] != nums[nums[i]])
-                swap(nums[i], nums[nums[i]]);
-            else
-                i++;
+        int slow = 0;
+        int fast = 0;
+        while(true){
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+            if(slow==fast) break;
         }
-        for(int i = 0; i<nums.size(); i++){
-            if(nums[i]!=i)
-                return nums[i];
+        fast = 0;
+        while(slow!=fast){
+            slow = nums[slow];
+            fast = nums[fast];
         }
-        return 0;
+        return slow;
     }
 };
