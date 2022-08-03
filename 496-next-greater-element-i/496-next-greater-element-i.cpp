42c4ed0625fe511cb7af44f02ff2372c6386e727
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-08-03 15:10:29 +0530

    Time: 8 ms (70.38%), Space: 9.1 MB (14.89%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> res(nums1.size(),-1);
-        for(int i = 0; i<nums1.size(); i++){
-            int j = 0;
-            while(nums1[i]!=nums2[j]) j++; 
-            while(j<nums2.size()){
-                if(nums2[j]>nums1[i]){ 
-                    res[i] = nums2[j];
-                    break;
-                }
-                j++;
-            }
+        stack<int> st;
+        vector<int> res;
+        map<int,int> mp;
+        for(int i = nums2.size()-1; i>=0; i--){
+            while(!st.empty() && st.top()<nums2[i]) st.pop();
+            if(st.empty()) mp[nums2[i]] = -1;
+            else mp[nums2[i]] = st.top();
+            st.push(nums2[i]);
         }
+        for(auto i : nums1) res.push_back(mp[i]);
         return res;
     }
 };
