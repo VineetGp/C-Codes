ff8059e94fe29f0d8a913e29778d66989f2b8207
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-09-22 19:54:40 +0530

    Time: 218 ms (8.09%), Space: 34.1 MB (37.98%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        priority_queue<pair<int, int>> pq;
-        int gap;
-        for(int i = 0; i<k; i++){
-            gap = abs(arr[i] -  x);
-            pq.push({gap, arr[i]});
-        }
-        for(int i = k; i<arr.size(); i++){
-            gap = abs(arr[i] - x);
-            if((pq.top().first == gap && pq.top().second > arr[i]) || pq.top().first > gap){
+        priority_queue<pair<int, int> > pq;
+        vector<int> ans;
+        for(int i{0};i<arr.size();i++){
+            pq.push(make_pair(abs(arr.at(i)-x),arr.at(i)));
+            if(pq.size()>k){
                 pq.pop();
-                pq.push({gap,arr[i]});
             }
         }
-        vector<int> res;
         while(!pq.empty()){
-            res.push_back(pq.top().second);
+            pair<int, int> pot = pq.top();
+            ans.push_back(pot.second);
             pq.pop();
         }
-        sort(res.begin(), res.end());
-        return res;
+        sort(ans.begin(),ans.end());
+        return ans;
     }
 };
