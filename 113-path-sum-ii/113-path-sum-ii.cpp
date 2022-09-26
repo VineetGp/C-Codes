f015dd971c1f0a0f4db79801a8ae25a999770f3f
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-09-26 18:53:02 +0530

    Time: 17 ms (63.52%), Space: 19.9 MB (79.83%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -9,28 +9,50 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+// class Solution {
+// public:
+    
+//     void solve(TreeNode* root, int targetSum, vector<vector<int>> &res, vector<int> dummy ){
+//         if(!root) return;
+        
+//         if(!root->left and !root->right and targetSum == root->val){
+//             dummy.push_back(root->val);
+//             res.push_back(dummy);
+//             return;
+//         }
+        
+//         dummy.push_back(root->val);
+//         solve(root->left, targetSum-root->val, res, dummy);
+//         // dummy.pop_back();
+//         solve(root->right, targetSum-root->val, res, dummy);
+//     }
+    
+//     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
+//         vector<vector<int>> res;
+//         vector<int> dummy;
+//         solve(root, targetSum, res, dummy);
+//         return res;
+//     }
+// };
+
+// OR
+
 class Solution {
 public:
-    
-    void solve(TreeNode* root, int targetSum, vector<vector<int>> &res, vector<int> dummy ){
-        if(!root) return;
+    vector<vector<int>> ans;
+    vector<int> v;
+    void dfs(TreeNode* root,int target){
+        if(!root)return;
+        v.push_back(root->val);
+        target-=root->val;
+        if(target==0 && !root->left && !root->right)ans.push_back(v);
+        dfs(root->left,target);
+        dfs(root->right,target);
+        v.pop_back();
         
-        if(!root->left and !root->right and targetSum == root->val){
-            dummy.push_back(root->val);
-            res.push_back(dummy);
-            return;
-        }
-        
-        dummy.push_back(root->val);
-        solve(root->left, targetSum-root->val, res, dummy);
-        // dummy.pop_back();
-        solve(root->right, targetSum-root->val, res, dummy);
     }
-    
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
-        vector<vector<int>> res;
-        vector<int> dummy;
-        solve(root, targetSum, res, dummy);
-        return res;
+        dfs(root,targetSum);
+        return ans;
     }
 };
