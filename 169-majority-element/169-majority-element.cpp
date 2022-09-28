4877e40f4f0b4c04b827b187fb9e1ae6f9767358
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-09-28 16:07:46 +0530

    Time: 42 ms (30.97%), Space: 19.6 MB (59.65%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,18 +1,14 @@
-/*
-https://leetcode.com/problems/majority-element/discuss/51613/O(n)-time-O(1)-space-fastest-solution
-https://www.cs.utexas.edu/~moore/best-ideas/mjrty/index.html
-*/
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         int count = 0, val = 0;
         for(int i = 0; i<nums.size(); i++){
-            if(count==0){
-                count++;
+            if(count == 0){
                 val = nums[i];
+                count++;
             }
-            else if(nums[i]==val) count++;
-            else count--;
+            else if(val != nums[i]) count--;
+            else count++;
         }
         return val;
     }
