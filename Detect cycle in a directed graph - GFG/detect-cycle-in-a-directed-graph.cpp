35e355be33326ecae5833c80e0828a1442192f44
VineetGp <97475154+VineetGp@users.noreply.github.com>
2022-10-20 16:39:24 +0530

    Added solution - LeetHub

--- Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
+++ Detect cycle in a directed graph - GFG/detect-cycle-in-a-directed-graph.cpp
@@ -5,71 +5,36 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
     
-//   private:
-//     bool check(int node, vector<int> adj[], vector<int>& vis, vector<int>& preVis){
-//         vis[node] = 1;
-//         preVis[node] = 1;
+  private:
+    bool check(int node, vector<int> adj[], vector<int>& vis, vector<int>& pathVis){
+        vis[node] = 1;
+        pathVis[node] = 1;
         
-//         for(auto it : adj[node]){
-//             if(!vis[it]){
-//                 if(check(it, adj, vis, preVis)) return true;
-//             }
-//             else if(preVis[node] == 1) return true;
-//         }
+        for(auto it : adj[node]){
+            if(!vis[it]){
+                if(check(it, adj, vis, pathVis)) return true;
+            }
+            else if(pathVis[it] == 1) return true;
+        }
         
-//         preVis[node] = 0;
-//         return false;
-//     }
+        pathVis[node] = 0;
+        return false;
+    }
     
-//   public:
-//     bool isCyclic(int V, vector<int> adj[]) {
-//         vector<int> vis (V,0);
-//         vector<int> preVis (V,0);
+  public:
+    bool isCyclic(int V, vector<int> adj[]) {
+        vector<int> vis (V,0);
+        vector<int> pathVis (V,0);
         
-//         for(int i = 0; i<V; i++){
-//             if(!vis[i]){
-//                 if(check(i, adj, vis, preVis)) return true;
-//             }
-//         }
-//         return false;
-//     }
-// };
-private:
-	bool dfsCheck(int node, vector<int> adj[], int vis[], int pathVis[]) {
-		vis[node] = 1;
-		pathVis[node] = 1;
-
-		// traverse for adjacent nodes
-		for (auto it : adj[node]) {
-			// when the node is not visited
-			if (!vis[it]) {
-				if (dfsCheck(it, adj, vis, pathVis) == true)
-					return true;
-			}
-			// if the node has been previously visited
-			// but it has to be visited on the same path
-			else if (pathVis[it]) {
-				return true;
-			}
-		}
-
-		pathVis[node] = 0;
-		return false;
-	}
-public:
-	// Function to detect cycle in a directed graph.
-	bool isCyclic(int V, vector<int> adj[]) {
-		int vis[V] = {0};
-		int pathVis[V] = {0};
-
-		for (int i = 0; i < V; i++) {
-			if (!vis[i]) {
-				if (dfsCheck(i, adj, vis, pathVis) == true) return true;
-			}
-		}
-		return false;
-	}
+        for(int i = 0; i<V; i++){
+            if(!vis[i]){
+                if(check(i, adj, vis, pathVis)) return true;
+            }
+        }
+        return false;
+    }
 };
+
 
 //{ Driver Code Starts.
 
